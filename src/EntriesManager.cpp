b55b17a05758ca7f967422fe41c9066b7132c318
vaedermakar <vaedermakar@mail.ru>
2021-05-14 18:06:28 +0300

    working on EntriesManager

--- src/EntriesManager.cpp
+++ src/EntriesManager.cpp
@@ -1,6 +1,30 @@
 #include "EntriesManager.h"
 
-EntriesManager::EntriesManager()
+#include "minizip-ng/mz.h"
+#ifdef HAVE_COMPAT
+#include "mz_compat.h"
+#endif
+#include "minizip-ng/mz_crypt.h"
+#include "minizip-ng/mz_os.h"
+#include "minizip-ng/mz_strm.h"
+#ifdef HAVE_BZIP2
+#include "minizip-ng/mz_strm_bzip.h"
+#endif
+#ifdef HAVE_PKCRYPT
+#include "minizip-ng/mz_strm_pkcrypt.h"
+#endif
+#include "minizip-ng/mz_strm_mem.h"
+#include "minizip-ng/mz_strm_os.h"
+#ifdef HAVE_WZAES
+#include "mz_strm_wzaes.h"
+#endif
+#ifdef HAVE_ZLIB
+#include "mz_strm_zlib.h"
+#endif
+#include "minizip-ng/mz_zip.h"
+
+
+EntriesManager::EntriesManager(const QString& zip_file_path)
 {
 
 }
--- src/EntriesManager.h
+++ src/EntriesManager.h
@@ -1,7 +1,23 @@
 #pragma once
 
+
+#include <QString>
+
 class EntriesManager
 {
+    QString _zip_file_path;
+    bool _mmap_use; // true
+
 public:
-    EntriesManager();
+    explicit EntriesManager(const QString& zip_file_path);
+    virtual ~EntriesManager();
+
+    // sets mz_zip_reader_open_file or mz_zip_reader_open_file_in_memory using
+    void setMmapUse(bool use);
+    bool getMmapUse()   const;
+
+    QMap<QString, QString> getAllEntries() const;
+
+
+
 };
